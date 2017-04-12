c6ce5cdced5d2b40a85002e514352b6d7531e4e8
brunoreili <brunoreili@gmail.com>
2017-04-11 22:43:41 -0300

    Finalizando Back-end - REST
    Implementando métodos POST, GET e DELETE

---
+++ CadastroDePiadas/src/main/java/entidade/Autor.java
@@ -0,0 +1,22 @@
+package entidade;
+
+public class Autor {
+    
+    private String nome;
+    private int nota;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+    
+}
